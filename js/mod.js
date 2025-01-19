11a1e8af50c900051a891f9773e5f159bbaecbff
doge68342 <fieldinghoffmeier@gmail.com>
2025-01-19 15:45:25 -0700

    Update mod.js
    upgrade

--- js/mod.js
+++ js/mod.js
@@ -40,8 +40,8 @@ function canGenPoints(){
 function getPointGen() {
 	if(!canGenPoints())
 		return new Decimal(0)
-
 	let gain = new Decimal(1)
+	if (hasUpgrade('b', 11)) gain = gain.times(2)
 	return gain
 }
 

43356d140fd9a6808d5756abea71986cfaa967fa
doge68342 <fieldinghoffmeier@gmail.com>
2025-01-19 15:19:25 -0700

    Update mod.js
    personalized the template

--- js/mod.js
+++ js/mod.js
@@ -1,7 +1,7 @@
 let modInfo = {
-	name: "The ??? Tree",
-	author: "nobody",
-	pointsName: "points",
+	name: "The Omega Tree",
+	author: "Doge68342",
+	pointsName: "Alpha",
 	modFiles: ["layers.js", "tree.js"],
 
 	discordName: "",

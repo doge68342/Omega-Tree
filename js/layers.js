19eac555a3cf429e76d42dc098aaa0778699825f
doge68342 <fieldinghoffmeier@gmail.com>
2025-01-19 16:07:04 -0700

    Update layers.js
    no

--- js/layers.js
+++ js/layers.js
@@ -37,11 +37,11 @@ addLayer("b", {
             cost: new Decimal(3),
         },
         13: {
-            title: "Ugraded Upgrade",
+            title: "Upgraded Upgrade",
             description: "Boost alpha gain based on beta",
             cost: new Decimal(5),
             effect() {
-                return player[this.layer].points.add(1).pow(0.5)
+                return player[this.layer].points.add(1).pow(1)
             },
             effectDisplay() { return format(upgradeEffect(this.layer, this.id))+"x" }, // Add formatting to the effect
         },
